49abc3d3784f96b7af5f9e2bf32d451061ee02f7
Samet Kula <113592491+SametKula@users.noreply.github.com>
2023-09-09 18:42:25 +0300

    Time: 0 ms (100.00%), Space: 40.1 MB (26.32%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,42 +1,24 @@
 class Solution {
-    int[] nums ;
-    Integer[] cache;
     public int combinationSum4(int[] nums, int target) {
-        this.nums = nums;
-        this.cache = new Integer[target + 1];
-        return helper(target);
+        int[] dp = new int[target + 1];
+
+        for (int i = 0; i < dp.length; i++)
+            dp[i] = -1;
+
+        return helper(nums, target, dp);
     }
-    private int helper(int curr){
-        if (curr == 0) return 1;
-        if (curr < 0) return 0;
-        if (cache[curr] != null ) return cache[curr];
+
+    private int helper(int[] nums, int target, int[] dp) {
+        if (target == 0) return 1;
+        if (dp[target] != -1) return dp[target];
+
         int result = 0;
+
         for (int i = 0; i < nums.length; i++) {
-            result += helper(curr - nums[i]);
+            if (target >= nums[i])
+                result += helper(nums, target - nums[i], dp);
         }
-        cache[curr] = result;
+        dp[target] = result;
         return result;
     }
-    // public int combinationSum4(int[] nums, int target) {
-    //     int[] dp = new int[target + 1];
-
-    //     for (int i = 0; i < dp.length; i++)
-    //         dp[i] = -1;
-
-    //     return helper(nums, target, dp);
-    // }
-
-    // private int helper(int[] nums, int target, int[] dp) {
-    //     if (target == 0) return 1;
-    //     if (dp[target] != -1) return dp[target];
-
-    //     int result = 0;
-
-    //     for (int i = 0; i < nums.length; i++) {
-    //         if (target >= nums[i])
-    //             result += helper(nums, target - nums[i], dp);
-    //     }
-    //     dp[target] = result;
-    //     return result;
-    // }
 }
