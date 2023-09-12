e77359c16b0cf16390a44ad111e8452b0bf8c0a6
Samet Kula <113592491+SametKula@users.noreply.github.com>
2023-09-12 14:21:08 +0300

    Time: 0 ms (100%), Space: 38.9 MB (96.76%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.java
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.java
@@ -1,42 +1,13 @@
 class Solution {
-    private int MOD = 1_000_000_007;
-    private long[][] memo;
-    
-    private long totalWays(int unpicked, int undelivered) {
-        if (unpicked == 0 && undelivered == 0) {
-            // We have completed all orders.
+    private int MOD = (int)1e9 + 7;
+
+    private long calculateOrdersCount(long remainingPairs) {
+        if (remainingPairs == 0)
             return 1;
-        }
-        
-        if (unpicked < 0 || undelivered < 0 || undelivered < unpicked) {
-            // We can't pick or deliver more than N items
-            // Number of deliveries can't exceed number of pickups 
-            // as we can only deliver after a pickup.
-            return 0;
-        }
-        
-        if (memo[unpicked][undelivered] != 0) {
-            // Return cached value, if already present. 
-            return memo[unpicked][undelivered];
-        }
-        
-        long ans = 0;
-        
-        // Count all choices of picking up an order.
-        ans += unpicked * totalWays(unpicked - 1, undelivered);
-        // Handle integer overflow.
-        ans %= MOD;
-        
-        // Count all choices of delivering a picked order.
-        ans += (undelivered - unpicked) * totalWays(unpicked, undelivered - 1);
-        // Handle integer overflow.
-        ans %= MOD;
-        
-        return memo[unpicked][undelivered] = ans;
+        long currentResult = calculateOrdersCount(remainingPairs - 1) * (2 * remainingPairs - 1) * remainingPairs % MOD;
+        return currentResult;
     }
-    
-    public int countOrders(int n) {
-        memo = new long[n + 1][n + 1];
-        return (int)totalWays(n, n);
+    public int countOrders(int numPairs) {
+        return (int)calculateOrdersCount(numPairs);
     }
 }
