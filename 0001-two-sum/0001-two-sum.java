97dc56759f03af54128432318aff667a4056dafb
Samet Kula <113592491+SametKula@users.noreply.github.com>
2024-06-28 17:16:49 +0300

    Time: 45 ms (29.87%), Space: 44.9 MB (34.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> hm = new HashMap<>();
+  /*      Map<Integer, Integer> hm = new HashMap<>();
+        System.gc();
         for (int i = 0; i < nums.length; i++) {
             if (hm.containsKey(target - nums[i])){
                 System.gc();
@@ -9,5 +10,19 @@ class Solution {
             hm.put(nums[i], i);
         }
         return new int[]{0,0};
+*/
+        for(int i = 0; i < nums.length - 1; i++) {
+            for(int j = i + 1; j < nums.length; j++){
+                if(i == j){
+                    continue;//ayni sayiyi kontrol etmesinin onune geciyoruz
+                }
+
+                if(nums[i] + nums[j] == target){// eger kontrol ettigimiz sayilar hedefi sagliyorsa cevabi donduruyoruz
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{0,0};
+
     }
 }
